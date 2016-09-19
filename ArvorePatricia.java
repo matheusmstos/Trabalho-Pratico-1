cf85ef334c02ca50858f9e0ff4cadcde8e61690b
NathaCarvalhais <natha626@yahoo.com.br>
2016-09-19 18:13:34 -0300

    Quase lá

--- ArvorePatricia.java
+++ ArvorePatricia.java
@@ -1,158 +1,127 @@
-public class ArvorePatricia
-{
-    private static abstract class PatNo {}
+package cap5;
+public class ArvorePatricia {
+  private static abstract class PatNo { }
+  private static class PatNoInt extends PatNo {
+    int index; PatNo esq, dir;
+  }  
+  private static class PatNoExt extends PatNo {
+    char chave;     //
+    int ocorrencias // Deve-se contabilizar as ocorrencias das palavras
+                    // Deve-se também afirmar qual é a posição de cada ocorrencia(Usando vector ou ArrayList)
+  }
+  
+  private PatNo raiz;
+  private int nbitsChave;
 
-        private static class PatNoInt extends PatNo
-        {
-            int index;       //nosso nó, indicando bit diferentes
-            PatNo esq, dir;  //e os nós a esq e dir
-        }
+// Converte a string passada por parametro para bytes
+   public byte converteChar(String chave,int i){
+	char caractere = chave.charAt(i);
+	byte converte = (byte) caractere;
+	return converte;
+  }
+  
+  // @{\it Retorna o i-\'esimo bit da chave k a partir da esquerda}@
+  private int bit (int i, char k) {
+    if (i == 0) return 0;
+    int c = (int)k;
+    for (int j = 1; j <= this.nbitsChave - i; j++) c = c/2;
+    return c % 2;
+  }
 
-        private static class PatNoExt extends PatNo
-        {
-            char chave; //palavra propriamente dita
-            //no nosso caso, tranformamos um char em int(16bits)
-        }
+  // @{\it Verifica se p \'e n\'o externo}@
+  private boolean eExterno (PatNo p) {    
+    Class classe = p.getClass ();
+    return classe.getName().equals(PatNoExt.class.getName());
+  }
 
-        private PatNo raiz;
-        private int nbitsChave;
+  private PatNo criaNoInt (int i, PatNo esq, PatNo dir) {
+    PatNoInt p = new PatNoInt ();
+    p.index = i; p.esq = esq; p.dir = dir;
+    return p;
+  }
 
-        //metodos privados
+  private PatNo criaNoExt (char k) {
+    PatNoExt p = new PatNoExt ();
+    p.chave = k;
+    return p;
+  }
+  
+  private void pesquisa (char k, PatNo t) {
+    if (this.eExterno (t)) {
+      PatNoExt aux = (PatNoExt)t;
+      if (aux.chave == k) System.out.println ("Elemento encontrado");
+      else System.out.println ("Elemento nao encontrado");
+    }
+    else { 
+      PatNoInt aux = (PatNoInt)t;
+      if (this.bit (aux.index, k) == 0) pesquisa (k, aux.esq);
+      else pesquisa (k, aux.dir);
+    }
+  }
 
-        //numero de bits por chave
-        public ArvorePatricia(int nbitsChave)
-        {
-            this.raiz = null;
-            this.nbitsChave = nbitsChave;
-        }
+  private PatNo insereEntre (char k, PatNo t, int i) {
+    PatNoInt aux = null; 
+    if (!this.eExterno (t)) aux = (PatNoInt)t;
+    if (this.eExterno (t) || (i < aux.index)) { // @{\it Cria um novo n\'o externo}@
+      PatNo p = this.criaNoExt (k);
+      if (this.bit (i, k) == 1) return this.criaNoInt (i, t, p);
+      else return this.criaNoInt (i, p, t);
+    }
+    else {
+      if (this.bit (aux.index, k) == 1) 
+        aux.dir = this.insereEntre (k, aux.dir, i);
+      else aux.esq = this.insereEntre (k, aux.esq, i);
+      return aux;
+    }
+  }
+  
+  private PatNo insere (char k, PatNo t) {
+    if (t == null) return this.criaNoExt (k);
+    else {
+      PatNo p = t;
+      while (!this.eExterno (p)) {
+        PatNoInt aux = (PatNoInt)p;
+        if (this.bit (aux.index, k) == 1) p = aux.dir; else p = aux.esq;
+      }
+      PatNoExt aux = (PatNoExt)p;
+      int i = 1; // @{\it acha o primeiro bit diferente}@
+      while ((i <= this.nbitsChave)&&
+             (this.bit (i, k) == this.bit (i, aux.chave))) i++;
+      if (i > this.nbitsChave) {
+        System.out.println ("Erro: chave ja esta na arvore"); 
+        return t;
+      }
+      else return this.insereEntre (k, t, i);
+    }
+  }
+  
+  private void central (PatNo pai, PatNo filho, String msg) {
+    if (filho != null) {
+      if (!this.eExterno (filho)) {
+        PatNoInt aux = (PatNoInt)filho; 
+        central (filho, aux.esq, "ESQ");
+        if (pai != null)
+          System.out.println ("Pai: "+ ((PatNoInt)pai).index + " " + msg+ " Int: " + aux.index);
+        else System.out.println ("Pai: "+ pai + " " + msg+ " Int: " + aux.index);
+        central (filho, aux.dir, "DIR");
+      } else {
+        PatNoExt aux = (PatNoExt)filho;
+        if (pai != null)
+          System.out.println ("Pai: "+ ((PatNoInt)pai).index + " " + msg+ " Ext: " + aux.chave);
+        else System.out.println ("Pai: "+ pai + " " + msg+ " Ext: " + aux.chave);
+      }
+    }
+  }
 
-        /*----------Métodos Auxiliares-----------*/
+  public void imprime () {
+    this.central (null, this.raiz, "RAIZ");
+  }
 
-        //retorna o i-esimo bit da chave k a partir da esquerda
-        private int bit(int i, char k)
-        {
-            if(i == 0)
-            return 0;
-
-            int c = (int)k;
-
-            for(int j = 1; j = this.nbitsChave -i; j++)
-            c = c/2;
-
-            return c%2;
-        }
-
-        //verifica se p é nó externo
-        private boolean eExterno(PatNo p)
-        {
-            Class classe = p.getClass();
-
-            return classe.getName().equals(PatNoExt.class.getName());
-        }
-
-        //metodo para criar nó interno
-        private PatNo criaNoInt(int i, PatNo esq, PatNo dir)
-        {
-            PatNoInt p = new PatNoInt();
-            p.index = i;
-            p.esq = esq;
-            p.dir = dir;
-            return p;
-        }
-
-        //metodo pra criar no eExterno
-        private PatNo criaNoExt(char k)
-        {
-            PatNoExt p = new PatNoExt();
-            p.chave = k;
-            return p;
-        }
-
-        /*----------Pesquisa-----------*/
-
-        public void pesquisa(char k)
-        {
-            this pesquisa(k, this.raiz);
-        }
-
-        private void pesquisa(char k, PatNo t)
-        {
-            if(this.eExterno(t))
-            {
-                PatNoExt aux = (PatNoExt)t;
-                if(aux.chave == k) System.out.println("Elemento encontrado");
-                else System.out.println("Elemento não encontrado")
-            }
-            else
-            {
-                PatNoInt aux = (PatNoInt)t;
-                if(this.bit(aux.index, k) == 0) pesquisa(k, aux.esq);
-                else pesquisa(k, aux.dir);
-            }
-
-        }
-
-        /*----------Insere-----------*/
-
-        public void insere(char k)
-        {
-            this.raiz = this.insere(k, this.raiz);
-        }
-
-        private PatNo insere(char k, PatNo t, int i)
-        {
-            PatNoInt aux = null;
-
-            if(!this.eExterno(t)) aux = (PatNoInt)t;
-
-            if(this.eExteno(t) || (i < aux.index))
-            {
-                //Cria um novo nó eExterno
-                PatNo p = this.criaNoExt(k);
-                if(this.bit(i, k) == 1) return this.criaNoInt(i, t, p);
-                else return this.criaNoInt(i, p, t);
-            }
-            else
-            {
-                if(this.bit(aux.index, k) == 1)
-                    aux.dir = this.insereEntre(k, aux.dir, i);
-                else
-                    aux.esq = this.insereEntre(k, aux.esq, i);
-
-                return aux;
-            }
-
-        }
-
-        private PatNo insere(char k, PatNo t)
-        {
-            if(t == null)
-                return this.criaNoExt(k);
-            else
-            {
-                PatNo p =t;
-                while(!this.eExterno(p))
-                {
-                    PatNoInt aux = (PatNoInt)p;
-                    if(this.bit(aux.index, k) == 1)
-                        p = aux.dir;
-                    else p = aux.esq;
-                }
-
-                PatNoExt aux = (PatNoExt)p;
-                int i = 1; //acha o primeiro bit diferente
-
-                while((i <= this.nbitsChave)
-                && (this.bit(i, k) == this.bit(i, aux.chave))) i++;
-
-                if(i > this.nbitsChave)
-                {
-                    System.out.println("Erro: chave ja existe nessa arvore");
-                    return t;
-                }
-                else return this.insereEntre(k, t, i);
-            }
-        }
-
-
+  public ArvorePatricia (int nbitsChave) {
+    this.raiz = null; this.nbitsChave = nbitsChave; 
+  }
+  
+  public void pesquisa (char k) { this.pesquisa (k, this.raiz); }
+  
+  public void insere (char k) { this.raiz = this.insere (k, this.raiz); } 
 }
---
+++ Main.java
@@ -0,0 +1,58 @@
+package TrabalhoPratico;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+	
+	/*	
+	    int palavras = 1;
+	    String nome,arroz;
+	    String escolhida;
+	    while(!= EOF){
+	        // Parte da Extração
+	        nome = resultado_extração
+	        if(nome.length() <=  16){
+	            for(i=0;i<=nome.length();i++){
+	                arroz[i] = nome[i];
+	            }
+	            (i=nome.length(); i<=16;i++){
+	                arroz[i] = ' ';
+	            }
+	        }
+	        insere(arroz);
+	        Extrai palavra
+	        palavra++;
+	        else{
+	           nome[17] = '\0'
+	           inserir(nome);
+	        }
+	        System.out.println("------------------------------MENU---------------------------");
+	        System.out.println("Digite 1 para escolher uma palavra para ser pesquisada");
+	        System.out.println("Digite 0 para sair da aplicação");
+	        switch(op){
+	            case '1':
+	            {
+	                escolhida = in.nextString();
+	                Pesquisa(escolhida);
+	                System.out.println("A palavra escolhida foi:" + escolhida);
+	                System.out.println("Palavra com " + ocorrencias +  " ocorrencias);
+	                Imprimir vector ou array list 
+	            }
+	        }
+	        
+	    }
+	*/
+	    /* --------------- STEPS --------------------
+	        Criar contador de palavras (Começa no 1)
+	        Extrair a palavra do texto
+	        Aumentar pra ela ter 16 letras /Caso for maior que 16 letras particionar em duas
+	        Inserir ela na Árvore Patricia
+	        Adicionar o contador de palavras (palavras ++)
+	        Assim vai até o final do arquivo
+	        Qual palavra deseja buscar no texto?
+	        
+	    */	
+		
+	}
+
+}
