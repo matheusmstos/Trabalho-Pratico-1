32848bbcb2294c2e9f27c22458aa76685eeb94a0
Aluno <aluno@decom-OptiPlex-780>
2016-09-22 17:39:30 -0300

    Main esquematizada

--- Main.java
+++ Main.java
@@ -1,58 +1,66 @@
-package TrabalhoPratico;
 import java.util.*;
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main (String[] args){
+	int op,arroz;
+	String digitada
+	Scanner entrada = new Scanner(System.in);	
+	try {
+	   ExtraiPalavra palavras = new ExtraiPalavra(args [0], args[1]);
+	   String palavra = null;
+	   int i = 1;
+	   while ((palavra = palavras.proximaPalavra())!=null){
+		//Magia negra acontece aqui
+	   }
+	   palavras.fecharArquivos();
+	} catch (Exception e) {
+	   System.out.println (e.getMessage ());
+	   }
+	do{
+		System.out.println("------------------------------MENU------------------------------");
+		System.out.println("Digite 1 para escolher uma palavra para ser pesquisada");
+		System.out.println("Digite 0 para sair da aplicação");
+		op = entrada.nextInt();
 	
-	/*	
-	    int palavras = 1;
-	    String nome,arroz;
-	    String escolhida;
-	    while(!= EOF){
-	        // Parte da Extração
-	        nome = resultado_extração
-	        if(nome.length() <=  16){
-	            for(i=0;i<=nome.length();i++){
-	                arroz[i] = nome[i];
-	            }
-	            (i=nome.length(); i<=16;i++){
-	                arroz[i] = ' ';
-	            }
-	        }
-	        insere(arroz);
-	        Extrai palavra
-	        palavra++;
-	        else{
-	           nome[17] = '\0'
-	           inserir(nome);
-	        }
-	        System.out.println("------------------------------MENU---------------------------");
-	        System.out.println("Digite 1 para escolher uma palavra para ser pesquisada");
-	        System.out.println("Digite 0 para sair da aplicação");
-	        switch(op){
-	            case '1':
-	            {
-	                escolhida = in.nextString();
-	                Pesquisa(escolhida);
-	                System.out.println("A palavra escolhida foi:" + escolhida);
-	                System.out.println("Palavra com " + ocorrencias +  " ocorrencias);
-	                Imprimir vector ou array list 
-	            }
-	        }
-	        
-	    }
-	*/
-	    /* --------------- STEPS --------------------
-	        Criar contador de palavras (Começa no 1)
-	        Extrair a palavra do texto
-	        Aumentar pra ela ter 16 letras /Caso for maior que 16 letras particionar em duas
-	        Inserir ela na Árvore Patricia
-	        Adicionar o contador de palavras (palavras ++)
-	        Assim vai até o final do arquivo
-	        Qual palavra deseja buscar no texto?
-	        
-	    */	
+		switch(op){
+
+		    case '1':
+			{
+			  do{
+			    System.out.println("Digite a palavra a ser pesquisada e imprimir suas aplicações");
+			    digitada = entrada.nextString();
+			    // Chama pesquisa
+			    
+			    System.out.println("Digite 1 para pesquisar uma nova palavra ou digite qualquer número para encerrar sua aplicação");
+			    arroz = entrada.nextInt();
+			   }while(arroz == 1);
+			   break;
+			}
+		    default:
+			System.out.println("Aplicação terminada com sucesso");
+			break;
+
+		}
+		System.out.println("Digite 1 para continuar na aplicação ou digite qualquer número para encerra-la");
+		op = entrada.nextInt();
 		
-	}
+	}while(op == 1);	
+      	
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
