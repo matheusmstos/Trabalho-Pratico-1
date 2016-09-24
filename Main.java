80c3c5f8b9c7e80a50f9704dd0aaf521ef64230d
Matheus Santos <aluno@decom-OptiPlex-780>
2016-09-23 21:01:22 -0300

    Pronto

--- Main.java
+++ Main.java
@@ -1,48 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-class Extrair {
-    String token;
-    PushbackReader arqTxt;
-
-    boolean isAlpha(char ch) {
-        if(ch >= 'A' && ch <= 'z')
-            return true;
-        return false;
-    }
-
-    public Extrair(String arqName) throws FileNotFoundException {
-        this.arqTxt = new PushbackReader(new FileReader(arqName));
-    }
-
-    public String nextToken() throws IOException {
-        int estado = 0;
-        String palavra = "";
-        char ch;
-        while(estado != -1) {
-            ch = (char) this.arqTxt.read();
-            switch(estado) {
-                case 0:
-                    if(ch == -1)
-                        return null;
-                    else if(!isAlpha(ch) && ch != '-')
-                        continue;
-                    estado = 1;
-                    palavra += ch;
-                    break;
-                case 1:
-                    if(!isAlpha(ch) && ch != '-') {
-                        estado = -1;
-                        this.arqTxt.unread(ch);
-                    }
-                    palavra += ch;
-                    break;
-            }
-        }
-        return palavra;
-    }
-
-}
 
 public class Main {
 
@@ -52,28 +10,28 @@ public class Main {
 	 String digitada;
      ArvorePatricia ar = new ArvorePatricia(128);
      Scanner entrada = new Scanner(System.in);
-
+     String aux;
 	 try {
-        //ExtraiPalavra palavras = new ExtraiPalavra(args [0], args[1]);
-        Extrair palavras = new Extrair(args[0]);
+        ExtraiPalavra palavras = new ExtraiPalavra(args [0], args[1]);
 	    String palavra = null;
 	    int i = 1;
-	    while ((palavra = palavras.nextToken()) != null){
-                System.out.println("teste: " + palavra);
-
-                 ar.insere(palavra);
-                 /*
-                    List
-                 */
+	    while ((palavra = palavras.proximaPalavra()) != null){
+                //System.out.println("Palavra: " + palavra);
+                 ar.insere(palavra.toLowerCase(),i);
                  i++;
 	    }
-	    //palavras.fecharArquivos();
+	    palavras.fecharArquivos();
 	 } catch (Exception e) {
 	    System.out.println (e.getMessage ());
 	    }
-     ar.imprime();
-     /*
-     do{
+        aux = ar.converteString("sem");
+        ar.pesquisa(aux);
+
+        //ar.imprime();
+     }
+}
+
+/*     do{
 	 	System.out.println("------------------------------MENU------------------------------");
 	 	System.out.println("Digite 1 para escolher uma palavra para ser pesquisada");
 	 	System.out.println("Digite 0 para sair da aplicação");
@@ -81,12 +39,14 @@ public class Main {
 
 	 	switch(op){
 
-	 	    case '1':
+	 	    case 1:
 	 		{
 	 		  do{
 	 		    System.out.println("Digite a palavra a ser pesquisada e imprimir suas aplicações");
-	 		    digitada = entrada.nextString();
-	 		            //Chama pesquisa
+                entrada.nextInt();
+	 		    digitada = entrada.nextLine();
+                        a = ar.converteString(digitada);
+	 		            ar.pesquisa(a);
 
 	 		    System.out.println("Digite 1 para pesquisar uma nova palavra ou digite qualquer número para encerrar sua aplicação");
 	 		    arroz = entrada.nextInt();
@@ -102,17 +62,5 @@ public class Main {
 	 	op = entrada.nextInt();
 
 	 }while(op == 1);
-
-
-
-
-    ar.insere("maria");
-    ar.insere("predrin");
-    ar.insere("natha");
-    ar.insere("rodrigo");
-    ar.insere("grafos");
-    ar.imprime();
-    */
-
     }
-}
+    */
